494952a654cdf36630a0302a62860a9c4f2fb8a8
Anna Zheng <azheng38@wisc.edu>
2025-09-25 22:27:52 -0400

    Added the rest of the functionality. Files with certain extensions are moved to the corresponding folders defined by the extensions dictionary. If the folder did not previously exist, it will be made.

--- organize_desktop.py
+++ organize_desktop.py
@@ -1,6 +1,7 @@
 import os # to be able to access our directories
 import shutil # for moving files around (needed to organize our desktop)
 
+# modify as necessary for different scenarios!
 extensions = {
     ".jpg": "Images",
     ".png": "Images",
@@ -8,7 +9,9 @@ extensions = {
     ".mp4": "Videos",
     ".blend": "Blender",
     ".blend1": "Blender Backups",
-    ".mp3": "Music"
+    ".mp3": "Music",
+    ".c": "C Programs",
+    ".pdf": "PDFs"
 }
 
 def main():
@@ -22,8 +25,29 @@ def main():
     else:
         print("Path provided is not a directory. Unable to organize.")
 
-def organize_desktop(path):
-    for filename in os.listdir(path):
-        print(filename)
+def organize_desktop(directory_path):
 
+    for filename in os.listdir(directory_path):
+        
+        original_file_path = os.path.join(directory_path, filename)
+        if not os.path.isfile(original_file_path):
+            print(f"Skip {filename}. This is a folder.")
+
+        # the extension of the file
+        extension = os.path.splitext(filename)[1].lower()
+
+        if extension in extensions:
+
+            # from the file's extension, get the corresponding folder name
+            folder_name = extensions[extension]
+            
+            folder_path = os.path.join(directory_path, folder_name)
+            # only creates the folder if it doesn't exist yet
+            os.makedirs(folder_path, exist_ok=True)
+
+            destination_path = os.path.join(folder_path, filename)
+            # move the file into the new folder
+            shutil.move(original_file_path, destination_path)
+        else:
+            print(f"Skip {filename}. It has an unknown extension.")
 main()
